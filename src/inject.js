a92c68c698d89fa4f4bda948e3df920ed0659acd
Shain Singh <shain.singh@gmail.com>
2022-04-06 17:07:34 +1000

    initial commit - CSP errors on load

---
+++ src/inject.js
@@ -0,0 +1,12 @@
+// this is the code which will be injected into a given page...
+
+(function() {
+
+    var s = document.createElement('script');
+    s.src = "https://us.gimp.zeronaught.com/__imp_apg__/js/volt-volterra_savqwqpc-26768971.js";
+    (document.head || document.documentElement).appendChild(s);
+
+	alert('inserted F5 Client Side Defence script');
+
+})();
+
